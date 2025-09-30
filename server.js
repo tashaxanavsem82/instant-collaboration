6ae37d3d13ece38485122aa9ff01f573e6115600
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-30 09:43:19 -0500

    Add CORS support to the Express server.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+app.use(cors());
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
